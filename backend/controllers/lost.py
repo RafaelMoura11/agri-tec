ba9bef5a0372f0acad002dee54eef16004465c1f
Rafael <rafaelmoura7033@gmail.com>
2022-08-25 12:25:15 -0300

    Adicionando metodo put na rota

--- backend/controllers/lost.py
+++ backend/controllers/lost.py
@@ -13,6 +13,17 @@ lost_ns = server.lost_ns
 lost_schema = LostSchema()
 lost_list_schema = LostSchema(many=True)
 
+lost = lost_ns.model('Lost', {
+    "name": fields.String(),
+    "email": fields.String(),
+    "cpf": fields.String(),
+    "latitude": fields.Float(),
+    "longitude": fields.Float(),
+    "type": fields.String(),
+    "date": fields.Date(),
+    "event": fields.String()
+})
+
 class Lost(Resource):
     def get(self, id):
         lost_data = LostModel.find_by_id(id)
@@ -26,6 +37,26 @@ class Lost(Resource):
             lost_data.delete_from_db()
             return 'Perda deletada!', 204
         return {'message': ITEM_NOT_FOUND}, 404
+
+    @lost_ns.expect(lost)
+    def put(self, id):
+        lost_data = LostModel.find_by_id(id)
+        lost_json = request.get_json()
+
+        if lost_data:
+            lost_data.name = lost_json['name']
+            lost_data.email = lost_json['email']
+            lost_data.cpf = lost_json['cpf']
+            lost_data.latitude = lost_json['latitude']
+            lost_data.longitude = lost_json['longitude']
+            lost_data.type = lost_json['type']
+            lost_data.date = lost_json['date']
+            lost_data.event = lost_json['event']
+        else:
+            lost_data = lost_schema.load(lost_json)
+
+        lost_data.save_to_db()
+        return lost_schema.dump(lost_data), 200
 
 class LostList(Resource):
     @lost_ns.doc('Get all the Items')
