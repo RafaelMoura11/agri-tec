0ac296c0f728cd3596c8f6f59fd44a5c5c6fee5e
Rafael <rafaelmoura7033@gmail.com>
2022-08-25 12:13:39 -0300

    Adicionando metodo delete em Lost

--- backend/controllers/lost.py
+++ backend/controllers/lost.py
@@ -6,7 +6,7 @@ from schemas.lost import LostSchema
 
 from server.instance import server
 
-ITEM_NOT_FOUND = "Book not found."
+ITEM_NOT_FOUND = "Perda não encontrada."
 
 lost_ns = server.lost_ns
 
@@ -19,3 +19,24 @@ class Lost(Resource):
         if lost_data:
             return lost_schema.dump(lost_data)
         return {'message': ITEM_NOT_FOUND}, 404
+    
+    def delete(self, id):
+        lost_data = LostModel.find_by_id(id)
+        if lost_data:
+            lost_data.delete_from_db()
+            return 'Perda deletada!', 204
+        return {'message': ITEM_NOT_FOUND}, 404
+
+class LostList(Resource):
+    @lost_ns.doc('Get all the Items')
+    def get(self):
+        return lost_list_schema.dump(LostModel.find_all()), 200
+
+    @lost_ns.doc('Create an Item')
+    def post(self):
+        lost_json = request.get_json()
+        lost_data = lost_schema.load(lost_json)
+
+        lost_data.save_to_db()
+
+        return lost_schema.dump(lost_data), 201
