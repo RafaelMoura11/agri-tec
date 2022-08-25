10e60f8ef30919ef265f0cd9f735e7aa59ff7669
Rafael <rafaelmoura7033@gmail.com>
2022-08-25 12:10:07 -0300

    Adicionando nova rota em app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from marshmallow import ValidationError
+from controllers.lost import Lost, LostList, lost_ns
 
 from ma import ma
 from db import db
@@ -12,6 +13,10 @@ app = server.app
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+
+api.add_resource(Lost, '/losts/<int:id>')
+api.add_resource(LostList, '/losts')
 
 
 if __name__ == '__main__':
