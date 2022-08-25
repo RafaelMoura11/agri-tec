6261d11ba9daef63d26d5665c2aa9b4d646a0b32
Rafael <rafaelmoura7033@gmail.com>
2022-08-25 18:56:17 -0300

    Criando interface das perdas

---
+++ frontend/src/interfaces/LostInterface.ts
@@ -0,0 +1,13 @@
+interface LostInterface {
+    id?: number | string;
+    name: string;
+    email: string;
+    cpf: string;
+    latitude: number;
+    longitude: number;
+    type: string;
+    date: string;
+    event: string;
+}
+
+export default LostInterface;
