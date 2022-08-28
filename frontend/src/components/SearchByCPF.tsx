ef3ed0a55fb182d8e1365f3d5518e697596cc8a6
Rafael <rafaelmoura7033@gmail.com>
2022-08-28 14:31:13 -0300

    Ajustando tamanho e titulo da search bar

--- frontend/src/components/SearchByCPF.tsx
+++ frontend/src/components/SearchByCPF.tsx
@@ -4,11 +4,13 @@ import { Context } from '../context/Provider'
 const SearchByCPF: React.FC = () => {
     const { searchByCPF, setSearchByCPF } = useContext(Context);
     return (
-        <form className="form-inline d-flex justify-content-center md-form form-sm mt-0">
-            <i className="fas fa-search" aria-hidden="true"></i>
-            <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Busque por nome"
-            aria-label="Search" defaultValue={ searchByCPF } onChange={ (e: any) => setSearchByCPF(e.target.value) } />
-        </form>
+        <div className="container">
+            <form className="form-inline d-flex justify-content-center md-form form-sm mt-0" id="search-bar">
+                <i className="fas fa-search" aria-hidden="true"></i>
+                <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Busque por CPF"
+                aria-label="Search" defaultValue={ searchByCPF } onChange={ (e: any) => setSearchByCPF(e.target.value) } />
+            </form>
+        </div>
     )
 }
 
